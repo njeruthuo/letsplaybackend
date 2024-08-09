008c05db1c365db7848c385167ef8376d56c8fa2
njeruthuo <njeruthelearner@gmail.com>
2024-08-09 12:57:56 +0300

    basedir error

--- yard/wsgi.py
+++ yard/wsgi.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 
 import os
 from whitenoise import WhiteNoise
+from settings import BASE_DIR
 
 from django.core.wsgi import get_wsgi_application
 
